2e2bfd23c83e897c2daf162f97750d57b5ce0521
aidanoneill <agoneill@tcd.ie>
2015-03-17 10:47:09 +0000

    1

--- R/getEntities.numbers.string.R
+++ R/getEntities.numbers.string.R
@@ -1,5 +1,6 @@
 getEntities.numbers.string <- 
   function(s){
+    require("NLP")
     g = strsplit(gsub("[[:alpha:]]", "", s), " ")
     l = lapply(g[[1]], function(x) ifelse(nchar(gsub("[[:punct:]]", "", x))==0, TRUE, FALSE))
     return(g[[1]][which(l ==FALSE)])
